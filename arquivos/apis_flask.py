dd6e1d74e8ca3fd86f5cee9a106cb12d0286729a
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-01-21 17:46:53 -0300

    mudanças de arquivos e schemas

--- arquivos/apis_flask.py
+++ arquivos/apis_flask.py
@@ -1,21 +1,33 @@
+import os
 import uuid
-from db import stores,items
-from flask import Flask,request
-from flask_smorest import abort,Api
+import models
+from db import db
+from flask import Flask
+from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
 #flask run PARA RODAR O ARQUIVO, O CMD TEM QUE ESTAR APONTANDO PARA A PASTA QUE TEM O ARQUIVO
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config['API_TITLE'] = "Stores Rest api"
-app.config["API_VERSION"] = "V1"
-app.config["OPENAPI_VERSION"]= "3.0.3"
-app.config["OPENAPI_URL_PREFIX"]= "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"]= "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"]= "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config['API_TITLE'] = "Stores Rest api"
+    app.config["API_VERSION"] = "V1"
+    app.config["OPENAPI_VERSION"]= "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"]= "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"]= "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"]= "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"]=db_url or os.getenv("DATABASE_URL","sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+    db.init_app(app)
 
-api = Api(app)
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    api = Api(app)
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+
+    return app
--- arquivos/resources/store.py
+++ arquivos/resources/store.py
@@ -10,6 +10,7 @@ blp = Blueprint("stores",__name__,description="Operations on stores")
 
 @blp.route("/stores/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200,StoreSchema)
     def get(self,store_id):
         try:
             return stores[store_id]
@@ -25,10 +26,12 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200,StoreSchema)
     def get(self):
         return {"stores":list(stores.values())}
 
     @blp.arguments(StoreSchema)
+    @blp.response(201,StoreSchema)
     def post(self,store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
@@ -37,4 +40,4 @@ class StoreList(MethodView):
         store_id = uuid.uuid4().hex
         new_store = {**store_data,"id":store_id}
         stores[store_id] = new_store
-        return new_store, 201
+        return new_store
